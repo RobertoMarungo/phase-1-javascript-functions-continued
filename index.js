cf2d4cbeda20fcba5c6154a0b8408e20714860d4
Roberto Marungo <robmarungo.dev@gmail.com>
2023-03-19 20:52:08 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+const saturdayFun = (funActivity = "roller-skate") =>
+    `This Saturday, I want to ${funActivity}!`;
+
+const mondayWork = (task = "go to the office") =>
+    `This Monday, I will ${task}.`;
+
+const wrapAdjective =
+    (value = "*") =>
+    (msg = "special") =>
+        `You are ${value}${msg}${value}!`;
